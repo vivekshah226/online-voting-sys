5489cfdda07209f20c51b08b30db6fe35a541eab
Mayukh Bhattacharyya <60193612+Mayukh135@users.noreply.github.com>
2026-05-14 01:12:13 +0530

    fixes

--- mini online voting sys/voters/index.php
+++ mini online voting sys/voters/index.php
@@ -4,107 +4,84 @@
     
 ?>
 
-    <div clas="row my-3">
+    <div class="row my-3">
             <div class="col-12">
-                <h3> Voters Panel </h3>
+                <h3>Voters Panel</h3>
 
             <?php
                 $fetchingActiveElections = mysqli_query($db, "SELECT * FROM elections WHERE status = 'Active'") or
                 die(mysqli_error($db));
                 $totalActiveElections = mysqli_num_rows($fetchingActiveElections);
 
-                if($totalActiveElections > 0){
-
-                    while($data = mysqli_fetch_array($fetchingActiveElections)){
-
-                        $election_id = $data['id'];
-                        $election_topic = $data['election_topic'];
-                    ?>
                     <?php
                     }
 
                 ?>
-                    <table class="table">
-                    <thead>
-                        <tr>
-                            <th colspan="4" class="bg-green text-white"><h5> ELECTION TOPIC: <?php echo 
-                           strtoupper($election_topic); ?></h5> </th>
-                            
-                        </tr>
-                        <tr>
-                            <th> Photo </th>
-                            <th> Candidate Details </th>
-                            <th> No of Votes </th>
-                            <th> Action </th>
-                        </tr>
-            </thead>
-            <tbody>
-                <?php
-                    $fetchingCandidates = mysqli_query($db, "SELECT * FROM candidates WHERE election_id = '$election_id'") or
-                    die(mysqli_error($db));
-                    while($candidateData = mysqli_fetch_assoc($fetchingCandidates)){
-                        
-                        $candidate_id = $candidateData['id'];
-                        $candidate_photo = $candidateData['candidate_photo'];
-
-                        //Fetching candidate Votes
-                        $fetchingVotes = mysqli_query($db, "SELECT * FROM votings WHERE candidate_id = '" .$candidate_id . "'")
-                         or die(mysqli_error($db));
-                        $totalVotes = mysqli_num_rows($fetchingVotes);
-
-                        echo $_SESSION['user_id'];
-
-                     
-                    ?>
-                        <tr>
-                            <td> <img src="<?php echo $candidate_photo; ?>" class="candidate_photo"></td>
-                            <td><?php echo "<b>" . $candidateData['candidate_name'] . "</b><br />". 
-                            $candidateData['candidate_details']; ?></td>
-                            <td><?php echo $totalVotes ?></td>
-                            <td> 
+                    <div class="table-responsive">
+                        <table class="table">
+                        <thead>
+                            <tr>
+                                <th colspan="4" class="bg-green text-white"><h5 class="mb-0">ELECTION TOPIC: <?php echo strtoupper($election_topic); ?></h5></th>
+                            </tr>
+                            <tr>
+                                <th>Photo</th>
+                                <th>Candidate Details</th>
+                                <th>No of Votes</th>
+                                <th>Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
                             <?php
-                                $checkIfVoted = mysqli_query($db, "SELECT * FROM votings WHERE voters_id = '" .$_SESSION['user_id'] 
-                                . "' AND election_id = '". $election_id ."'")or die(mysqli_error($db));
-                                $isVoteCasted = mysqli_num_rows($checkIfVoteCasted);
+                                $fetchingCandidates = mysqli_query($db, "SELECT * FROM candidates WHERE election_id = '$election_id'") or die(mysqli_error($db));
+                                while($candidateData = mysqli_fetch_assoc($fetchingCandidates)){
+                                    
+                                    $candidate_id = $candidateData['id'];
+                                    $candidate_photo = $candidateData['candidate_photo'];
 
-                                
-                                if($isVoteCasted > 0){
-                                    $voteCastedData = mysqli_fetch_assoc($checkIfVoteCasted);
-                                    $voteCastedCandidateId = $voteCastedData['candidate_id'];
-
-                                    if($voteCastedCandidateId == $candidate_id){
-                                    ?>
-                                      <img src="../assets/images/vote'png " width="100px;">
-                                    <?php
-                                    }  
-
-                                }else {
-
+                                    $fetchingVotes = mysqli_query($db, "SELECT * FROM votings WHERE candidate_id = '" . $candidate_id . "'") or die(mysqli_error($db));
+                                    $totalVotes = mysqli_num_rows($fetchingVotes);
 
                                 ?>
-                                 <button class="btn btn-md btn-success onclick=CasteVote(<?php echo 
-                            $election_id; ?>, <?php echo $candidate_id;?>, <?php echo $_SESSION['user_id']; ?>)"> Vote </button>
-                                <?php
-                                
-                                }
-            
-                            
-                            
-                            
-                            ?>    
-                                
-                         </td>   
-                        </tr>
-                    <?php
-                    }
-                ?>
-                    
-            </tbody>
-        </table>
+                                <tr>
+                                    <td><img src="<?php echo $candidate_photo; ?>" class="candidate_photo" alt="Candidate photo"></td>
+                                    <td><?php echo "<b>" . $candidateData['candidate_name'] . "</b><br />" . $candidateData['candidate_details']; ?></td>
+                                    <td><?php echo $totalVotes; ?></td>
+                                    <td>
+                                    <?php
+                                        $checkIfVoted = mysqli_query($db, "SELECT * FROM votings WHERE voters_id = '" . $_SESSION['user_id'] . "' AND election_id = '" . $election_id . "'") or die(mysqli_error($db));
+                                        $isVoteCasted = mysqli_num_rows($checkIfVoted);
 
-                    
+                                        if($isVoteCasted > 0){
+                                            $voteCastedData = mysqli_fetch_assoc($checkIfVoted);
+                                            $voteCastedCandidateId = $voteCastedData['candidate_id'];
+
+                                            if($voteCastedCandidateId == $candidate_id){
+                                            ?>
+                                                <img src="../assets/images/vote.png" width="100" alt="Vote casted">
+                                            <?php
+                                            }
+                                        }else {
+                                    ?>
+                                        <button type="button" class="btn btn-md btn-success" onclick="CasteVote(<?php echo $election_id; ?>, <?php echo $candidate_id; ?>, <?php echo $_SESSION['user_id']; ?>)">Vote</button>
+                                    <?php
+                                        }
+                                    ?>    
+                                    </td>
+                                </tr>
+                                <?php
+                                }
+                            ?>
+                        </tbody>
+                    </table>
+                    </div>
+
                 <?php
-                
+                }else{
+                    echo "No any active elections.";
+                }
+            
+            
+            ?>
 
                 }else{
                     echo "No any active elections.";
@@ -119,21 +96,19 @@
 </div>
 </div>
      <script>
-        const CasteVote = (election_id, customer_id, voters_id) => {
+        const CasteVote = (election_id, candidate_id, voters_id) => {
             $.ajax({
                 type: "POST",
                 url: "inc/ajaxCalls.php",
-                 data: "e_id=" + election_id + "
-                    &c_id=" + customer_id + "&v_id=" + voters_id,
+                data: "e_id=" + election_id + "&c_id=" + candidate_id + "&v_id=" + voters_id,
                 success: function(response){
-                if(response == "success"){
-                    location.assign("index.php?voteCasted=1");
-                }else{
-                    location.assign("index.php?voteNotCasted=1");
+                    if(response == "success"){
+                        location.assign("index.php?voteCasted=1");
+                    }else{
+                        location.assign("index.php?voteNotCasted=1");
+                    }
                 }
-        }
-
-});
+            });
         }
      </script>
 
