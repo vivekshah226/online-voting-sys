03794c92ecbba1c7d74bec762a89281bb44c066e
alexpriyamborah50-glitch <alexpriyamborah50@gmail.com>
2026-05-13 19:28:16 +0530

    pp

--- mini online voting sys/admin/inc/add_elections.php
+++ mini online voting sys/admin/inc/add_elections.php
@@ -1,1 +1,24 @@
-Testing...
+<div class="row my-3">
+    <div class="col-4">
+        <h3>Add New Election</h3>
+        <form method="POST">
+            <div class="form-group">
+                <input type="text" name="election_topic" placeholder="Election Topic" class="form-control" required />
+            </div>
+            <div class="form-group">
+                <input type="number" name="number_of_candidates" placeholder="No of Candidates" class="form-control" required />
+            </div>
+            <div class="form-group">
+                <input type="text" onfocus="this.type='Date'" name="staring_date" placeholder="Starting Date" class="form-control" required />
+            </div>
+            <div class="form-group">
+                <input type="text" onfocus="this.type='Date'" name="ending_date"  placeholder="Ending Date" class="form-control" required />
+            </div>
+            <input type="submit" value="Add Election" name="addElectionBtn" class="btn btn-success" />
+        </form>
+    </div>
+
+    <div class="col-8">
+        <h3>Upcoming Elections</h3>
+    </div>
+</div>    
